a6d5771132c05d66705a2bacb5743b19e4a3748d
VitusSeed <vseed@mail.ru>
2024-02-03 06:21:39 +0300

    test

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -2,6 +2,14 @@ package ru.yandex.practicum.filmorate.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+
 @RestController
 public class FilmController {
+    private int id;
+    private String name;
+    private String description;
+    private LocalDateTime releaseDate;
+    private Duration duration;
 }
